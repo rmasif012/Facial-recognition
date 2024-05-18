c5f74d9a75fba44289738b541b14240d7419bda3
rmasif012 <asifrm364@gmail.com>
2024-05-18 23:45:37 +0300

    files uploaded

--- multiple.py
+++ multiple.py
@@ -5,11 +5,11 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # Function to detect faces in an image
 def detect_faces(image):
-    # Convert into grayscale
+
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # Detect faces
+
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-    # Draw rectangle around the faces
+
     for (x, y, w, h) in faces:
         cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
     return image
@@ -18,9 +18,9 @@ def detect_faces(image):
 def process_single_image(image_path):
     # Read the input image
     img = cv2.imread(image_path)
-    # Detect faces in the image
+
     result_image = detect_faces(img)
-    # Display the output
+
     cv2.imshow("Detected Faces", result_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
